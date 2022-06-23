386438dcde0e8860ef9ff6caff1ea961999c088a
Max van der Sluis <max@tanqyou.com>
2022-06-23 15:34:42 +0200

    add config loading

---
+++ src/MVDSAstConverterBundle.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace MVDS\AstConverterBundle;
+
+use MVDS\AstConverterBundle\DependencyInjection\MVDSAstConverterExtension;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MVDSAstConverterBundle extends Bundle {}
